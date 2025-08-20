4d94c3157982b6a4875dfddd562a16325a96b1c7
main <checkout>
2025-08-20 15:26:39 -0500

    added jacl.h back

--- lib.c
+++ lib.c
@@ -2,38 +2,10 @@
 #ifndef JACL_C
 #define JACL_C
 
-#include <jsio.h>
-#include <assert.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fenv.h>
-#include <float.h>
-#include <inttypes.h>
-#include <iso646.h>
-#include <limits.h>
-#include <locale.h>
-#include <math.h>
-#include <setjmp.h>
-#include <signal.h>
-#include <stdalign.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdnoreturn.h>
-#include <string.h>
-#include <tgmath.h>
-#include <time.h>
-#include <threads.h>
-#include <uchar.h>
-#include <wchar.h>
-#include <wctype.h>
-
 // Don't do this at home children!
-// But the compiler build it fast this way
+#include <jacl.h>
 #include <stlib.c>
 #include <jsio.c>
+// It's the fastest compile
 
 #endif /* JACL_C */
