ec86d8e623f717731d106ba7a2c9cf73be4e553d
Micon Frink <frink@frinknet.com>
2026-04-22 00:12:11 -0500

    fixed typos

--- jaclib.c
+++ jaclib.c
@@ -3,12 +3,12 @@
 
 /**
  * NOTE: This is the only C file needed to build the entire library if you
- * insist on building the library spearately to link with your project.
+ * insist on building the library separately to link with your project.
  *
  * This makes it at worst a footnote in your makefile and at best an example
- * implementation of how easy it is to use this thing. For more exmples and
+ * implementation of how easy it is to use this thing. For more examples and
  * the ubiquitous hello.c and others ee the examples/ directory.
  */
 
-//include the entire library
+//include the core only (everything else is static inline)
 #include <static.h>
